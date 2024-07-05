f32a10080eee1e0b86d5fcb4626adc0dae721725
PVTBrowsing <60885158+PVTBrowsing@users.noreply.github.com>
2024-07-05 17:59:01 +0100

    Add project files.

---
+++ DiscordBot/config/JSONReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.config
+{
+    internal class JSONReader
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+        
+        public async Task ReadJSON()
+        {
+            using (StreamReader sr = new StreamReader("config.json"))
+            {
+
+            }
+        }
+    }
+    internal sealed class JSONSTructure //sealed = disables class inheritance, this will store what the above class reads in and store it in a secured location
+    {
+        public string token { get; set; }
+        public string prefix { get; set; }
+    }
+}
