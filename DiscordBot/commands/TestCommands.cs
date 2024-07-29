f008e1addb9b20517fb55da80cca12084ea260e5
PVTBrowsing <60885158+PVTBrowsing@users.noreply.github.com>
2024-07-29 16:39:31 +0100

    embed msg test pass

--- DiscordBot/commands/TestCommands.cs
+++ DiscordBot/commands/TestCommands.cs
@@ -1,5 +1,6 @@
 ﻿using DSharpPlus.CommandsNext;
 using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,9 +13,26 @@ namespace DiscordBot.commands
     public class TestCommands: BaseCommandModule // inherits base class structure
     {
         [Command("test")]
-        public async Task command1(CommandContext context)// CommandContext accesses discord text
+        public async Task Speak(CommandContext context)// CommandContext accesses discord text
         {
             await context.Channel.SendMessageAsync($"Hello {context.User.Username}");
+        }
+        [Command("add")]
+        public async Task Add(CommandContext context, int num1,int num2 )
+        {
+            int result = num1 + num2;
+            await context.Channel.SendMessageAsync(result.ToString());
+        }
+        [Command("embed")]
+        public async Task EmbedMsg(CommandContext context)
+        {
+            var msg = new DiscordMessageBuilder()
+                .AddEmbed(new DiscordEmbedBuilder()
+                .WithTitle("Test Message - Embed")
+                .WithDescription($"Executed by {context.User.Username}")
+                .WithColor(DiscordColor.Blurple));
+
+            await context.Channel.SendMessageAsync(msg);
         }
     }
 }
